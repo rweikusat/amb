f81fa3e48c94b86444ede7a23567720824e3e335
Rainer Weikusat <rainerweikusat@virginmedia.com>
2025-05-01 12:49:25 +0100

    iterative implementation

---
+++ amb-iter.c
@@ -0,0 +1,113 @@
+/*
+  iterative implementation of https://rosettacode.org/wiki/Amb
+*/
+#include <alloca.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static char *articles[] = { "the", "that", "an", 0 };
+static char *nouns[] = { "frog", "elephant", "thing", 0};
+static char *verbs[] = { "walked", "treaded", "grows", 0 };
+static char *adverbs[] = { "slowy", "quickly", 0 };
+
+static char **words[] = {
+    articles,
+    nouns,
+    verbs,
+    adverbs,
+    0
+};
+
+/*
+ * condition:
+ * every word but the first starts with the letter
+ * the word before it ended with
+ */
+static int l_eq_f(char *w0, char *w1)
+{
+    unsigned len;
+
+    len = strlen(w0);
+    return len ? w0[len - 1] == *w1 : !*w1;
+}
+
+static int chained(char **sentence)
+{
+    char *cur, *next;
+
+    next = *sentence;
+    while ((cur = next, cur) && (next = *++sentence, next))
+        if (!l_eq_f(cur, next)) return 0;
+
+    return 1;
+}
+
+/*
+ * exhaustive search engine ("amb")
+ */
+static char **amb(char ***words, int (*pred)(char **))
+{
+    char **res, ***pos, **posl, *cur;
+    unsigned level, depth;
+
+    depth = 0;
+    while (words[depth]) ++depth;
+    if (!depth) return 0;
+
+    pos = alloca((depth - 1) * sizeof(*pos));
+    res = malloc((depth + 1) * sizeof(*res));
+    res[depth] = 0;
+    level = 0;
+    --depth;
+
+    do {
+        while (level < depth) {
+            pos[level] = words[level];
+            res[level] = *pos[level];
+
+            ++level;
+        }
+
+        posl = words[level];
+        while (cur = *posl, cur) {
+            res[level] = cur;
+            if (pred(res)) return res;
+
+            ++posl;
+        }
+
+        while (level) {
+            --level;
+
+            cur = *++pos[level];
+            if (cur) {
+                res[level] = cur;
+                break;
+            }
+        }
+
+        ++level;
+    } while (cur);
+    
+    return 0;
+}
+
+/*
+ * main
+ */
+int main(void)
+{
+    char **res;
+    
+    res = amb(words, chained);
+    if (res) {
+        fputs(*res, stdout);
+        while (*++res) printf(" %s", *res);
+        putchar('\n');
+    } else {
+        puts("no solution");
+    }
+    
+    return 0;
+}
